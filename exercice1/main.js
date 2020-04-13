466b1bd99a09b21150bde5e8b2ff7bcb29057c63
sitayebsofiane <sitayebsofiane51@gmail.com>
2020-04-13 17:42:19 +0200

    bruno

--- exercice1/main.js
+++ exercice1/main.js
@@ -1,11 +1,22 @@
 //Insert the main title
-var mainTitle = 
+var mainTitle = document.getElementById("mainTitle");
+mainTitle.textContent="bruno";
 
 //Insert the subtitle
-var subTitle = 
+var subTitle = document.getElementById("subTitle");
+subTitle.textContent="bruno";
+
 
 //Get all the contents elements and set a bleu background
-var contents =
+var contents = document.querySelectorAll(".content");
+contents.forEach(element => element.style.backgroundColor="blue");
 
 //Function to delete article on click
-function deleteContent()
+var bruno = 0;
+ function deleteContent(){
+    if(bruno%2===0)
+        document.querySelector('article').style.display='none';
+    else
+        document.querySelector('article').style.display='block';
+    bruno++;
+ }
